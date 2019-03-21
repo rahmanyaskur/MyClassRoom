fb5275595d2dca28fc7130b9749c9e84109f3937
rahmanyaskur <rahmanyaskur@gmail.com>
2019-03-21 13:37:37 +0700

    firstcommit

---
+++ app/src/main/java/com/example/android/myclassroom/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.myclassroom;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread(){
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
